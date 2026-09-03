ab7787ede988ae1589398e8d3d754b220ed4d438
bijeshsingha <singhabijesh7@gmail.com>
2026-09-03 18:06:57 +0530

    feat: install Google Analytics gtag G-85NLSNH147

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -120,6 +120,21 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${cormorant.variable} ${inter.variable}`}>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-85NLSNH147"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-85NLSNH147');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
